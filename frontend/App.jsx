747731e90c77758b2a82cac471838b4169e4de27
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-04 12:12:13 -0500

    Chore: Add basic configuration with webpack

---
+++ frontend/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../assets/stylesheets/main.scss';
+
+ReactDOM.render(
+  <h1>Kitter!</h1>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const VENDOR_LIBS = ["react", "react-dom"];
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css"
+  });
+
+module.exports = {
+    entry: {
+      bundle: './frontend/App.jsx',
+      vendor: VENDOR_LIBS,
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "[name].[chunkhash].js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.scss$/,
+          use: extractSass.extract({
+              use: [{
+                  loader: "css-loader"
+              }, {
+                  loader: "sass-loader"
+              }],
+              fallback: "style-loader"
+          })
+        }
+      ]
+    },
+    plugins: [
+      extractSass,
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ["vendor", "manifest"]
+      }),
+      new HtmlWebpackPlugin({
+        title: "Kitter",
+        filename: "index.html",
+        template: "./public/index.html"
+      })
+    ],
+    resolve: {
+      extensions: [".js", ".jsx"]
+    }
+};
