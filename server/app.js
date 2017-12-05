e722ffb24886f2cea5dda7b313528b253ee0e395
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-05 16:23:42 -0500

    Chore: Add passport js config for sign up and login components

--- server/app.js
+++ server/app.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const path = require('path');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const app = express();
+const User = require('./models/user');
 
+// Require Routes
+const indexRoutes = require('./routes/index');
+
+mongoose.connect('mongodb://localhost/Kitter', {useMongoClient: true});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+// PASSPORT CONFIG
+app.use(require('express-session')({
+  secret: 'Kitter is fun!',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use('/api', indexRoutes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
