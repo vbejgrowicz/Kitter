2ac49ca8615e4d66832b78357dacd7aafcf350b6
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-05 16:24:28 -0500

    Feature: Add user model and backend routes

---
+++ server/routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+const User = require('../models/user');
+
+// SIGN UP ROUTE
+router.post('/signup', (req, res) => {
+  const newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) {
+      res.send({ success: false, message: `${err.message}`});
+    } else {
+      passport.authenticate('local')(req, res, () => {
+        res.send({ success: true, message: `${user.username} was created!`});
+      });
+    }
+  });
+});
+
+// login logic
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}), (req, res) => {
+});
+
+module.exports = router;
