d6e2b05e826c6601f3175c8b8dc2f5af18a89e70
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-04 23:40:05 -0500

    Chore: Add seed data for featured posts on landing page

--- frontend/components/LandingPage/LandingPageIndex.jsx
+++ frontend/components/LandingPage/LandingPageIndex.jsx
@@ -2,8 +2,28 @@ import React from 'react';
 import '../../../assets/stylesheets/LandingPage.scss';
 import LoginTile from './LoginTile';
 
-class LandingPage extends React.Component {
+const featuredPosts = [
+  {
+    text: 'A cat can jump even 7 times as high as it is tall.',
+    image: 'https://images.unsplash.com/photo-1475518112798-86ae358241eb?auto=format&fit=crop&w=1950&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
+    author: 'Cat-Facts'
+  },
+  {
+    text: 'It is estimated that cats can make over 60 different sounds.',
+    image: 'https://images.unsplash.com/photo-1471874276752-65e2d717604a?auto=format&fit=crop&w=1950&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
+    author: 'Cat-Facts'
+  },
+  {
+    text: 'Should I knock over the christmas tree or nap all day?',
+    image: 'https://images.unsplash.com/photo-1498336179775-9836baef8fdf?auto=format&fit=crop&w=1950&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
+    author: 'Bad-Kitty'
+  }
+];
 
+class LandingPage extends React.Component {
+  // componentDidMount() {
+  //   console.log('fetch default post data');
+  // }
   render() {
     return (
       <div>
