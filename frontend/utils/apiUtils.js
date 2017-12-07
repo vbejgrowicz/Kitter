21b3b232f04f04ca526969aa163e7a42cf16a3d7
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-06 21:32:12 -0500

    Fix: Fix API call to send cookies back from the request

--- frontend/utils/apiUtils.js
+++ frontend/utils/apiUtils.js
@@ -1,4 +1,9 @@
+export const checkUser = () => {
+  return fetch('/api/user', { method: 'GET', credentials: 'same-origin'})
+  .then(resp => resp.json());
+}
+
 export const signUp = (newUser) => {
-  return fetch('/api/signup', { method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(newUser)})
+  return fetch('/api/signup', { method: 'POST', credentials: 'same-origin', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(newUser)})
   .then(resp => resp.json());
 }
