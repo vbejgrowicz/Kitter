c9e760ea2efb9ea4237484c19d59a550e27914f7
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-09 19:00:19 -0500

    Feature: Add ignore events to modals to avoid closing on click inside

--- frontend/components/Header/NavBar.jsx
+++ frontend/components/Header/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../../../assets/stylesheets/NavBar.scss';
+import '../../../assets/stylesheets/NavBar/NavBar.scss';
 import UserDropdown from './UserDropdown';
 import MeowModal from './MeowModal';
 
@@ -13,7 +13,7 @@ class NavBar extends React.Component {
     }
   }
 
-  toggleState(selected) {
+  handleClick(selected) {
     if (!this.state[selected]) {
       this.setState({
         User: false,
@@ -25,23 +25,33 @@ class NavBar extends React.Component {
     });
   }
 
+  ignoreClose(event) {
+    event.stopPropagation();
+  }
+
   render() {
     return (
-      <div id="nav">
-        <div className="link-container">
-          <div className="left">
-            <Link to="/"><i className="fa fa-home" aria-hidden="true"></i></Link>
-          </div>
-          <div className="center">
-            <Link to="/"><div className="brand-icon"></div></Link>
-          </div>
-          <div className="right">
-            <i className="fa fa-user-circle" aria-hidden="true" onClick={this.toggleState.bind(this, 'User')}></i>
-            <button className="link" onClick={this.toggleState.bind(this, 'Meow')}>Meow</button>
+      <div>
+        <div id="nav">
+          <div className="link-container">
+            <div className="left">
+              <Link to="/"><i className="fa fa-home" aria-hidden="true"></i></Link>
+            </div>
+            <div className="center">
+              <Link to="/"><div className="brand-icon"></div></Link>
+            </div>
+            <div className="right">
+              <i className="fa fa-user-circle" aria-hidden="true" onClick={this.handleClick.bind(this, 'User')}></i>
+              <button onClick={this.handleClick.bind(this, 'Meow')}>Meow</button>
+            </div>
           </div>
         </div>
-        <UserDropdown show={this.state.User} onClose={this.toggleState.bind(this, 'User')} />
-        <MeowModal show={this.state.Meow} onClose={this.toggleState.bind(this, 'Meow')} />
+        {this.state.User && (
+          <UserDropdown onClose={this.handleClick.bind(this, 'User')} ignoreClose={this.ignoreClose.bind(this)} />
+        )}
+        {this.state.Meow && (
+          <MeowModal onClose={this.handleClick.bind(this, 'Meow')} ignoreClose={this.ignoreClose.bind(this)} />
+        )}
       </div>
     );
   }
