37133caefc1683c2b249f5eec0c16937a77bafba
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-11 17:04:04 -0500

    Feature: Add links to user dropdown

--- frontend/components/Header/UserDropdown.jsx
+++ frontend/components/Header/UserDropdown.jsx
@@ -1,16 +1,52 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logOutUser } from '../../actions/AuthActions';
+import '../../../assets/stylesheets/NavBar/UserDropdown.scss';
 
 class UserDropdown extends React.Component {
 
   render() {
+    const currentUser = this.props.AuthReducer;
     return (
-      <div onClick={this.props.onClose}>
-        <div onClick={this.props.ignoreClose}>
-          User
+      <div id="user-dropdown" onClick={this.props.onClose}>
+        <div id="dropdown" onClick={this.props.ignoreClose}>
+          <ul>
+            <li className="user-info" >
+              <Link to={`/${currentUser.username}`} onClick={this.props.onClose}>
+                <div className="user">
+                  {currentUser.name}
+                  <p className="username">@{currentUser.username}</p>
+                </div>
+              </Link>
+            </li>
+            <hr />
+            <li className="profile link">
+              <Link to={`/${currentUser.username}`} onClick={this.props.onClose}>
+              <div><i className="fa fa-user-o" aria-hidden="true"></i> Profile</div>
+            </Link>
+            </li>
+            <hr />
+            <li className="logout link">
+              <div>Log Out</div>
+            </li>
+          </ul>
         </div>
       </div>
     );
   }
 }
 
-export default UserDropdown;
+function mapStateToProps({ AuthReducer }) {
+  return { AuthReducer };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateUser: (user) => {
+      dispatch(logOutUser());
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserDropdown);
