7bffe84d3ea3399167430d5a2680f430b93c73ff
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-11 15:35:13 -0500

    Refactor: Refactor new post form into seperate component

---
+++ frontend/components/Posts/NewPostForm.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import '../../../assets/stylesheets/Posts/PostForm.scss';
+import { newPost } from '../../actions/PostActions';
+
+
+class NewPostForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: '',
+    };
+  }
+
+  handleSubmit() {
+    this.props.createPost(this.state.post);
+    this.props.onClose();
+  }
+
+  handleInput(e) {
+    this.setState({
+      post: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div id="post-form">
+        <textarea maxLength="140" placeholder="What's happening?" onChange={this.handleInput.bind(this)} />
+        <button disabled={!this.state.post} onClick={this.handleSubmit.bind(this)}>Meow</button>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createPost: (post) => {
+      dispatch(newPost(post));
+    },
+  };
+};
+
+export default connect(null, mapDispatchToProps)(NewPostForm);
