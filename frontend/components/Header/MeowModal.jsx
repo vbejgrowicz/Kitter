e0c12dcf957033cd29297588ee492c27f9be2965
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-11 12:07:13 -0500

    Feature: Add function to handle create post in modal

--- frontend/components/Header/MeowModal.jsx
+++ frontend/components/Header/MeowModal.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import '../../../assets/stylesheets/NavBar/MeowModal.scss';
+import { newPost } from '../../actions/PostActions';
 
 
 class MeowModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: 'More Data',
+    };
+  }
+
+  handleSubmit() {
+    this.props.createPost(this.state.post);
+    this.props.onClose();
+  }
 
   render() {
     return (
       <div id="meow-modal" onClick={this.props.onClose}>
         <div id="modal" onClick={this.props.ignoreClose}>
-          Meow
+          MEOW Modal
+          <button onClick={this.handleSubmit.bind(this)}>Meow</button>
         </div>
       </div>
     );
   }
 }
 
-export default MeowModal;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    createPost: (post) => {
+      dispatch(newPost(post));
+    },
+  };
+};
+
+export default connect(null, mapDispatchToProps)(MeowModal);
