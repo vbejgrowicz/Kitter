7208c1181f649851c8dfbf5dbb6a35188aa72b48
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-13 10:22:40 -0500

    Feature: Add post form with focus and blur functions to user homepage

--- frontend/components/UserPage/UserHomepage.jsx
+++ frontend/components/UserPage/UserHomepage.jsx
@@ -3,18 +3,38 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/PostActions';
 import UserProfileCard from './UserProfileCard';
+import NewPostForm from '../Posts/NewPostForm';
 
 class UserPageIndex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focus: false
+    }
+  }
 
   componentDidMount() {
     this.props.fetchPosts();
+  }
+
+  handleFocus() {
+    this.setState({ focus: true });
+  }
+
+  handleBlur() {
+    this.setState({ focus: false });
   }
 
   render() {
     return (
       <div id="user-home-page">
         <UserProfileCard />
-        <div id="profile-feed">Profile Feed</div>
+        <div id="profile-feed">
+          <div onFocus={this.handleFocus.bind(this)} onBlur={this.handleBlur.bind(this)}>
+            <NewPostForm isFocused={this.state.focus} />
+          </div>
+
+        </div>
       </div>
     );
   }
