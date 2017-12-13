7f88818f7ff7c288dae89f22ec1fc5c90dc945f5
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-12 23:14:56 -0500

    Feature: Add user content to profile card in user homepage

--- frontend/components/UserPage/UserHomepage.jsx
+++ frontend/components/UserPage/UserHomepage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/PostActions';
 
@@ -9,10 +10,29 @@ class UserPageIndex extends React.Component {
   }
 
   render() {
+    const user = this.props.AuthReducer;
     return (
       <div id="user-home-page">
         <div id="profile-card">
-          Welcome {this.props.AuthReducer.name}!
+          <div className="content">
+            <div className="image">
+              <i className="fa fa-user-circle" aria-hidden="true"></i>
+            </div>
+            <Link className="account" to={`/${user.username}`}>
+              <div className="name">{user.name}</div>
+              <div className="username">@{user.username}</div>
+            </Link>
+            <div className="user-data">
+              <Link className="posts data" to={`/${user.username}`}>
+                <div className="title">Meows</div>
+                <div className="count">0</div>
+              </Link>
+              <Link className="following data" to={`/${user.username}/following`}>
+                <div className="title">Following</div>
+                <div className="count">0</div>
+              </Link>
+            </div>
+          </div>
         </div>
         <div id="profile-feed">Profile Feed</div>
       </div>
