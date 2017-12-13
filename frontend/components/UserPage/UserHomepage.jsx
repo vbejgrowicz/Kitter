9bf2b327eb3f97904e52ce61505d54d56de93d62
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-13 08:39:15 -0500

    Refactor: Refactor user profile card into seperate file

--- frontend/components/UserPage/UserHomepage.jsx
+++ frontend/components/UserPage/UserHomepage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/PostActions';
+import UserProfileCard from './UserProfileCard';
 
 class UserPageIndex extends React.Component {
 
@@ -10,38 +11,17 @@ class UserPageIndex extends React.Component {
   }
 
   render() {
-    const user = this.props.AuthReducer;
     return (
       <div id="user-home-page">
-        <div id="profile-card">
-          <div className="content">
-            <div className="image">
-              <i className="fa fa-user-circle" aria-hidden="true"></i>
-            </div>
-            <Link className="account" to={`/${user.username}`}>
-              <div className="name">{user.name}</div>
-              <div className="username">@{user.username}</div>
-            </Link>
-            <div className="user-data">
-              <Link className="posts data" to={`/${user.username}`}>
-                <div className="title">Meows</div>
-                <div className="count">0</div>
-              </Link>
-              <Link className="following data" to={`/${user.username}/following`}>
-                <div className="title">Following</div>
-                <div className="count">0</div>
-              </Link>
-            </div>
-          </div>
-        </div>
+        <UserProfileCard />
         <div id="profile-feed">Profile Feed</div>
       </div>
     );
   }
 }
 
-function mapStateToProps({ PostReducer, AuthReducer }) {
-  return { PostReducer, AuthReducer };
+function mapStateToProps({ PostReducer }) {
+  return { PostReducer };
 }
 
 const mapDispatchToProps = (dispatch) => {
---
+++ frontend/components/UserPage/UserProfileCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class UserProfileCard extends React.Component {
+
+  render() {
+    const user = this.props.AuthReducer;
+    return (
+      <div id="profile-card">
+        <div className="content">
+          <div className="image">
+            <i className="fa fa-user-circle" aria-hidden="true"></i>
+          </div>
+          <Link className="account" to={`/${user.username}`}>
+            <div className="name">{user.name}</div>
+            <div className="username">@{user.username}</div>
+          </Link>
+          <div className="user-data">
+            <Link className="posts data" to={`/${user.username}`}>
+              <div className="title">Meows</div>
+              <div className="count">0</div>
+            </Link>
+            <Link className="following data" to={`/${user.username}/following`}>
+              <div className="title">Following</div>
+              <div className="count">0</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ AuthReducer }) {
+  return { AuthReducer };
+}
+
+export default connect(mapStateToProps, null)(UserProfileCard);
