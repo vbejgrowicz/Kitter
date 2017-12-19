009c1e22008f8e022d80a7f0eca0e3eae9fbd3d7
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2017-12-19 12:14:23 -0500

    Feature: Add follow to profile and show button based on following status

--- frontend/components/UserProfile/ProfileUserInfo.jsx
+++ frontend/components/UserProfile/ProfileUserInfo.jsx
@@ -1,26 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { followUser } from '../../actions/AuthActions';
 
-function ProfileUserInfo({ UserReducer }) {
-  const { username, name, data } = UserReducer;
+function ProfileUserInfo({ UserReducer, AuthReducer, follow }) {
+  const {
+    id,
+    username,
+    name,
+    data,
+  } = UserReducer;
+  const user = { id, username, name };
+
+  const getFollowButton = () => {
+    const { list } = AuthReducer.user.following;
+    const isFollowing = list.find(follower => follower.id === user.id);
+    return !isFollowing ? (
+      <button onClick={() => follow(user)}>Follow</button>
+    ) : (
+      <button>Unfollow</button>
+    );
+  };
+
   return (
     <div id="profile-user-data">
       <div>{name}</div>
       <div>{username}</div>
       <div>Posts {data.posts}</div>
-      <button>Follow</button>
+      {getFollowButton()}
     </div>
   );
 }
 
 ProfileUserInfo.propTypes = {
   UserReducer: PropTypes.object.isRequired,
+  AuthReducer: PropTypes.object.isRequired,
+  follow: PropTypes.func.isRequired,
 };
 
 
-function mapStateToProps({ UserReducer }) {
-  return { UserReducer };
+function mapStateToProps({ UserReducer, AuthReducer }) {
+  return { UserReducer, AuthReducer };
 }
 
-export default connect(mapStateToProps, null)(ProfileUserInfo);
+const mapDispatchToProps = dispatch => (
+  {
+    follow: (user) => {
+      dispatch(followUser(user));
+    },
+  }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileUserInfo);
