870134aace04e6fba14f2fc229f66c8a5e5fb284
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-04 20:56:31 -0500

    Feature: Add dropzone component to upload user image modal

--- frontend/components/ProfileComponents/NewUserImageForm.jsx
+++ frontend/components/ProfileComponents/NewUserImageForm.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Dropzone from 'react-dropzone';
 class NewUserImageForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+    };
+    this.onDrop = this.onDrop.bind(this);
+  onDrop(file) {
+    this.setState({ file: file[0] });
+  }
   render() {
     return (
       <div id="user-image-modal">
+        <div className="modal">
+          <Dropzone
+            name="file"
+            className="image-drop"
+            accept="image/jpeg, image/png"
+            multiple={false}
+            onDrop={this.onDrop}
+          >
+            {this.state.file ? (
+              <img src={this.state.file.preview} alt="" />
+            ) : (
+              <p className="text">Drop a file here or select a file to upload</p>
+            )
+            }
+          </Dropzone>
+        </div>
       </div>
     );
   }
