ff2a04b1a0c32abfa56f7e00029432a0ec5da58e
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-05 09:52:51 -0500

    Fix: Fix aws storage to save images as public access for user viewing

--- server/routes/image.js
+++ server/routes/image.js
@@ -16,6 +16,8 @@ const upload = multer({
   storage: multerS3({
     s3,
     bucket: bucketName,
+    acl: 'public-read',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
     metadata: function (req, file, cb) {
       cb(null, {fieldName: file.fieldname});
     },
