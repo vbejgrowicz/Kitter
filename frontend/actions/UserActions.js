e4be78bc16168dba6e4e92f15d2c62f3ace1cb16
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-08 16:56:30 -0500

    Fix: Fix response if user not found

--- frontend/actions/UserActions.js
+++ frontend/actions/UserActions.js
@@ -38,7 +38,7 @@ export function getUser(username) {
     dispatch({ type: 'GET_USER_PROFILE' });
     findUser(username).then((response) => {
       const { user } = response;
-      if (user._id) {
+      if (user !== null) {
         dispatch(setUserData(user));
       } else {
         dispatch({ type: 'SET_USER_ERROR', error: true });
