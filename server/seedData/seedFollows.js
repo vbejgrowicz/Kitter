1fadac093dfe06da3477c122bb43224a00d2e708
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-09 17:20:01 -0500

    Feature: Add function to seed follow data

--- server/seedData/seedFollows.js
+++ server/seedData/seedFollows.js
@@ -17,6 +17,23 @@ const followData = [
 ];
 
 const seedFollows = () => {
+  followData.forEach((item) => {
+    User.findOne({ username: item.user }, (errOne, mainUser) => {
+      item.list.forEach((userItem) => {
+        User.findOne({ username: userItem }, (errTwo, followedUser) => {
+          const user = mainUser._id;
+          const following = followedUser._id;
+          const newFollow = { user, following };
+          Follow.create(newFollow, (error) => {
+            if (error) {
+              console.log(error);
+            }
+          });
+        });
+      });
+    });
+  });
+  console.log('All Follows Added');
 };
 
 module.exports = seedFollows;
