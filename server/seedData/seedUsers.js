0747f2487a730d80b299c07daef7410f598ebbc2
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-09 17:19:26 -0500

    Feature: Check for all users stored in db before doing callback function

--- server/seedData/seedUsers.js
+++ server/seedData/seedUsers.js
@@ -28,12 +28,18 @@ const userData = [
 ];
 
 const seedUsers = (addUserBasedData) => {
+  let usersAdded = 0;
   userData.forEach((seed) => {
     const newUser = new User({ username: seed.username, name: seed.name, image: seed.image });
     User.register(newUser, seed.password, (err) => {
       if (err) {
         console.log(err);
       } else {
+        usersAdded += 1;
+        if (usersAdded === userData.length) {
+          console.log('All Users Added');
+          addUserBasedData();
+        }
       }
     });
   });
