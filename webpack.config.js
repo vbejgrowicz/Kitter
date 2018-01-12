e9caf74fd6ceb9a85c716615b5dcdc5658be46af
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-12 12:33:18 -0500

    Chore: Add text area autosize react component to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const VENDOR_LIBS = ['react', 'react-dom', 'react-redux', 'react-router-dom', 'redux'];
+const VENDOR_LIBS = ['react', 'react-dom', 'react-redux', 'react-router-dom', 'redux', 'react-textarea-autosize'];
 
 const extractSass = new ExtractTextPlugin({
   filename: '[name].[contenthash].css',
