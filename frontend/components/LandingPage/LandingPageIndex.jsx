725feff730a23285af31abbcb4ec01e6bbc6e209
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-23 18:45:57 -0500

    Fix: Remove featured post data from landing page component

--- frontend/components/LandingPage/LandingPageIndex.jsx
+++ frontend/components/LandingPage/LandingPageIndex.jsx
@@ -8,24 +8,6 @@ import GuestLoginButton from '../AuthPages/GuestLoginButton';
 import { clearError } from '../../actions/AuthActions';
 import { fetchPosts } from '../../actions/PostActions';
 
-// const featuredPosts = [
-//   {
-//     text: 'A cat can jump even 7 times as high as it is tall.',
-//     image: 'https://images.unsplash.com/photo-1475518112798-86ae358241eb?auto=format&fit=crop&w=1950&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
-//     author: 'Cat-Facts',
-//   },
-//   {
-//     text: 'It is estimated that cats can make over 60 different sounds.',
-//     image: 'https://images.unsplash.com/photo-1471874276752-65e2d717604a?auto=format&fit=crop&w=1950&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
-//     author: 'Cat-Facts',
-//   },
-//   {
-//     text: 'Should I knock over the christmas tree or nap all day?',
-//     image: 'https://images.unsplash.com/photo-1498336179775-9836baef8fdf?auto=format&fit=crop&w=1950&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
-//     author: 'Bad-Kitty',
-//   },
-// ];
-
 class LandingPageIndex extends React.Component {
   componentDidMount() {
     const errorPage = this.props.AuthReducer.error.page;
