8d374e6d292614e2a2158b07e95072aa648d3548
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-24 16:00:00 -0500

    Chore: Remove webpack dev middleware func due to load issues in dev

--- server/app.js
+++ server/app.js
@@ -30,14 +30,14 @@ mongoose.connect(url, { useMongoClient: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV !== 'production') {
-  const webpackMiddleware = require('webpack-dev-middleware'); // eslint-disable-line global-require
-  const webpack = require('webpack'); // eslint-disable-line global-require
-  const webpackConfig = require('../webpack.config.js'); // eslint-disable-line global-require
-  app.use(webpackMiddleware(webpack(webpackConfig)));
-} else {
+// if (process.env.NODE_ENV !== 'production') {
+//   const webpackMiddleware = require('webpack-dev-middleware'); // eslint-disable-line global-require
+//   const webpack = require('webpack'); // eslint-disable-line global-require
+//   const webpackConfig = require('../webpack.config.js'); // eslint-disable-line global-require
+//   app.use(webpackMiddleware(webpack(webpackConfig)));
+// } else {
   app.use(express.static(path.resolve(__dirname, '..', 'build')));
-}
+// }
 
 seedDB();
 
@@ -61,11 +61,11 @@ app.use('/api/following/posts', followingPostRoutes);
 app.use('/api/follows', followerRoutes);
 app.use('/api/count', countRoutes);
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'build/index.html'));
   });
-}
+// }
 
 const port = process.env.PORT || 9000;
 
