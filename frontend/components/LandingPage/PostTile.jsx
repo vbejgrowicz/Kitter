7553193994db14b3cc8abd7b8852fd68aea21417
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-24 15:48:46 -0500

    Feature: Add linkify to landing page post tiles

--- frontend/components/LandingPage/PostTile.jsx
+++ frontend/components/LandingPage/PostTile.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Linkify from 'linkifyjs/react';
+import * as linkify from 'linkifyjs';
+import mention from 'linkifyjs/plugins/mention';
+
+mention(linkify);
 
 function PostTile({ post }) {
   return (
     <div className="tile">
       <div className="post-data">
-        <div className="post">{post.text}</div>
+        <Linkify className="post">{post.text}</Linkify>
         <div className="author">@{post.author.username}</div>
       </div>
       <div className="image" style={{ backgroundImage: `url(${post.author.image})` }} />
