ba178c6ee9f11919ef6fc39635661e8a6dbf9314
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-24 18:20:29 -0500

    Feature: Add redirect when post tile click and ignore if mention link

--- frontend/components/LandingPage/PostTile.jsx
+++ frontend/components/LandingPage/PostTile.jsx
@@ -6,17 +6,27 @@ import mention from 'linkifyjs/plugins/mention';
 
 mention(linkify);
 
-function PostTile({ post }) {
+function PostTile({ post }, contextTypes) {
+  const redirect = () => {
+    contextTypes.router.history.push(`/${post.author.username}`);
+  };
+  const linkProps = {
+    onClick: event => event.stopPropagation(),
+  };
   return (
-    <div className="tile">
+    <div className="tile with-cursor" onClick={redirect}>
       <div className="post-data">
-        <Linkify className="post">{post.text}</Linkify>
+        <Linkify className="post" options={{ attributes: linkProps }}>{post.text}</Linkify>
         <div className="author">@{post.author.username}</div>
       </div>
       <div className="image" style={{ backgroundImage: `url(${post.author.image})` }} />
     </div>
   );
 }
+
+PostTile.contextTypes = {
+  router: PropTypes.object.isRequired,
+};
 
 PostTile.propTypes = {
   post: PropTypes.object.isRequired,
