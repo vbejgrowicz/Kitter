a2657a46dbb60c14376642baa9e88aea9c1a191d
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-01-31 15:13:44 -0500

    Refactor: Refactor header buttons into separate components

--- frontend/components/ProfileComponents/Header/UserBar.jsx
+++ frontend/components/ProfileComponents/Header/UserBar.jsx
@@ -1,67 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import UserImage from '../Image/UserImage';
-import UserDropdown from './HeaderComponents/UserDropdown';
-import PostModal from './HeaderComponents/PostModal';
+import UserButton from './HeaderComponents/UserButton';
+import PostButton from './HeaderComponents/PostButton';
 
 function ignoreClose(event) {
   event.stopPropagation();
 }
-
-class UserBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      User: false,
-      Meow: false,
-    };
-  }
-
-  handleClick(selected) {
-    if (!this.state[selected]) {
-      this.setState({
-        User: false,
-        Meow: false,
-      });
-    }
-    this.setState({
-      [selected]: !this.state[selected],
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <div id="nav">
-          <div className="link-container">
-            <div className="left">
-              <NavLink exact to="/">
-                <div className="home">
-                  <i className="fa fa-home" aria-hidden="true" />
-                </div>
-              </NavLink>
-            </div>
-            <div className="center">
-              <div className="brand-icon" />
-            </div>
-            <div className="right">
-              <div onClick={() => this.handleClick('User')}>
-                <UserImage user={this.props.user} />
+function UserBar({ user }) {
+  return (
+    <div>
+      <div id="nav">
+        <div className="link-container">
+          <div className="left">
+            <NavLink exact to="/">
+              <div className="home">
+                <i className="fa fa-home" aria-hidden="true" />
               </div>
-              <button className="meow" onClick={() => this.handleClick('Meow')}>Meow</button>
-            </div>
+            </NavLink>
+          </div>
+          <div className="center">
+            <div className="brand-icon" />
+          </div>
+          <div className="right">
+            <UserButton user={user} ignoreClose={ignoreClose} />
+            <PostButton ignoreClose={ignoreClose} />
           </div>
         </div>
-        {this.state.User && (
-          <UserDropdown onClose={() => this.handleClick('User')} ignoreClose={ignoreClose} />
-        )}
-        {this.state.Meow && (
-          <PostModal onClose={() => this.handleClick('Meow')} ignoreClose={ignoreClose} />
-        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 UserBar.propTypes = {
